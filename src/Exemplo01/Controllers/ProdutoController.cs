af4c04b42eeb09e495fe045a777312fb98699217
DESKTOP-OL596BN\fabri <fabricioveronez@gmail.com>
2018-07-02 10:35:17 -0300

    Exemplo 01

---
+++ src/Exemplo01/Controllers/ProdutoController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Exemplo01.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProdutoController : ControllerBase
+    {
+
+        ILogger _log;
+
+        public ProdutoController(ILogger<ProdutoController> log)
+        {
+            this._log = log;
+        }
+
+        public ActionResult<IEnumerable<Produto>> GetAll()
+        {
+            this._log.LogInformation("Retornando a lista de produtos.");
+            return new List<Produto>();
+        }
+    }
+
+    public class Produto
+    {
+
+    }
+}
