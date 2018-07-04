1384a511172ba44659e8bd159a2d8cb466796954
DESKTOP-OL596BN\fabri <fabricioveronez@gmail.com>
2018-07-03 23:12:17 -0300

    Inclusão do projeto com o MongoContext e a entidade Produto.

---
+++ src/Compartilhado/MongoDBContext.cs
@@ -0,0 +1,17 @@
+﻿using MongoDB.Driver;
+
+namespace Compartilhado
+{
+    public class MongoDBContext
+    {
+
+        private IMongoDatabase MongoDataBase { get; set; }
+
+        public MongoDBContext()
+        {
+            this.MongoDataBase = (new MongoClient("mongodb://localhost:27017")).GetDatabase("admin");
+        }
+
+        public IMongoCollection<Produto> Produtos { get => this.MongoDataBase.GetCollection<Produto>("Produtos");  }
+    }
+}
